149926d2843069aa8a9c1cf2b0c3e840c6a55bfd
James Dunne <james.jdunne@gmail.com>
2015-10-30 07:39:52 -0500

    Rewrote to not use temp folder.

--- gifv/main.go
+++ gifv/main.go
@@ -7,10 +7,8 @@ import (
 	"net/http"
 	"os"
 	"path"
-	"path/filepath"
 	"strconv"
 	"sync"
-	"time"
 )
 
 var (
@@ -21,75 +19,45 @@ const thumbnail_dimensions = 200
 
 func store_folder() string { return base_folder + "/store" }
 func thumb_folder() string { return base_folder + "/thumb" }
-func tmp_folder() string   { return base_folder + "/tmp" }
 
-// Random number state.
-// We generate random temporary file names so that there's a good
-// chance the file doesn't exist yet - keeps the number of tries in
-// TempFile to a minimum.
-var rand uint32
-var randmu sync.Mutex
+func downloadFile(url string, id int64, ext string) (string, error) {
+	// Create a local file to download to:
+	img_name := strconv.FormatInt(id, 10)
+	store_path := path.Join(store_folder(), img_name+ext)
+	local_file, err := os.OpenFile(store_path, os.O_RDWR|os.O_CREATE|os.O_EXCL, 0600)
+	// File already exists:
+	if os.IsExist(err) {
+		var fi os.FileInfo
+		var head_rsp *http.Response
 
-func reseed() uint32 {
-	return uint32(time.Now().UnixNano() + int64(os.Getpid()))
-}
+		// Check file size:
+		fi, err = os.Stat(store_path)
+		file_size := fi.Size()
 
-func nextSuffix() string {
-	randmu.Lock()
-	r := rand
-	if r == 0 {
-		r = reseed()
-	}
-	r = r*1664525 + 1013904223 // constants from Numerical Recipes
-	rand = r
-	randmu.Unlock()
-	return strconv.Itoa(int(1e9 + r%1e9))[1:]
-}
-
-// TempFile creates a new temporary file in the directory dir
-// with a name beginning with prefix, opens the file for reading
-// and writing, and returns the resulting *os.File.
-// If dir is the empty string, TempFile uses the default directory
-// for temporary files (see os.TempDir).
-// Multiple programs calling TempFile simultaneously
-// will not choose the same file.  The caller can use f.Name()
-// to find the pathname of the file.  It is the caller's responsibility
-// to remove the file when no longer needed.
-func TempFile(dir, prefix, ext string) (f *os.File, err error) {
-	if dir == "" {
-		dir = os.TempDir()
-	}
-
-	nconflict := 0
-	for i := 0; i < 10000; i++ {
-		name := filepath.Join(dir, prefix+nextSuffix()+ext)
-		f, err = os.OpenFile(name, os.O_RDWR|os.O_CREATE|os.O_EXCL, 0600)
-		if os.IsExist(err) {
-			if nconflict++; nconflict > 10 {
-				rand = reseed()
+		// HEAD to get download file size:
+		head_rsp, err = http.Head(url)
+		if err == nil {
+			if head_rsp.ContentLength > 0 {
+				if file_size == head_rsp.ContentLength {
+					// File is same size; do nothing:
+					return store_path, nil
+				}
 			}
-			continue
+			// File is not same size; redownload:
+			local_file, err = os.OpenFile(store_path, os.O_RDWR|os.O_TRUNC|os.O_EXCL, 0600)
 		}
-		break
 	}
-	return
-}
+	if err != nil {
+		return "", err
+	}
+	defer local_file.Close()
 
-func downloadFile(url string) (string, error) {
 	// Do a HTTP GET to fetch the image:
 	img_rsp, err := http.Get(url)
 	if err != nil {
 		return "", err
 	}
 	defer img_rsp.Body.Close()
-
-	// Create a local temporary file to download to:
-	os.MkdirAll(tmp_folder(), 0755)
-	local_file, err := TempFile(tmp_folder(), "dl-", "")
-	if err != nil {
-		return "", err
-	}
-	defer local_file.Close()
 
 	// Download file:
 	_, err = io.Copy(local_file, img_rsp.Body)
@@ -100,18 +68,9 @@ func downloadFile(url string) (string, error) {
 	return local_file.Name(), nil
 }
 
-func moveToStoreFolder(local_path string, id int64, ext string) (err error) {
-	// Move and rename the file:
-	img_name := strconv.FormatInt(id, 10)
-	os.MkdirAll(store_folder(), 0755)
-	store_path := path.Join(store_folder(), img_name+ext)
-	if err = os.Rename(local_path, store_path); err != nil {
-		return
-	}
-	return nil
-}
-
 func main() {
+	os.MkdirAll(store_folder(), 0755)
+
 	file, err := os.Open("images.csv")
 	if err != nil {
 		fmt.Println(err)
@@ -139,47 +98,25 @@ func main() {
 		// Background-fetch the GIF, WEBM, and MP4 files:
 		wg := &sync.WaitGroup{}
 		wg.Add(3)
-		fetch_func := func(ext string, path *string) {
+		fetch_func := func(ext string) {
 			defer wg.Done()
 
-			// Fetch the GIF version:
+			// Fetch the file:
 			var err error
-			*path, err = downloadFile("http://i.imgur.com/" + imgur_id + ext)
+			path, err := downloadFile("http://i.imgur.com/"+imgur_id+ext, id, ext)
 			if err != nil {
 				fmt.Println(err)
 				return
 			}
+			fmt.Println(path)
 		}
 
-		var (
-			gif_file  string
-			webm_file string
-			mp4_file  string
-		)
-
-		go fetch_func(".gif", &gif_file)
-		go fetch_func(".webm", &webm_file)
-		go fetch_func(".mp4", &mp4_file)
+		go fetch_func(".gif")
+		go fetch_func(".webm")
+		go fetch_func(".mp4")
 
 		// Function to run after DB record creation:
 		// Wait for all files to download:
 		wg.Wait()
-
-		// Move temp files to final storage:
-		err = moveToStoreFolder(gif_file, id, ".gif")
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		err = moveToStoreFolder(webm_file, id, ".webm")
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		err = moveToStoreFolder(mp4_file, id, ".mp4")
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
 	}
 }
